d81d6ec8fc03f0692234c3bc55e3d99ac53e9504
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-31 22:34:56 +0200

    ruby: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: Redmi Note 12 Pro 5G Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Redmi Redmi Note 12 Pro 5G (ruby)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_ruby.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from ruby device
+$(call inherit-product, device/xiaomi/ruby/device.mk)
+
+PRODUCT_DEVICE := ruby
+PRODUCT_NAME := omni_ruby
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 12 Pro 5G
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ruby-user 12 SP1A.210812.016 V14.0.6.0.SMOMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/ruby_global/ruby:12/SP1A.210812.016/V14.0.6.0.SMOMIXM:user/release-keys
